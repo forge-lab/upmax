ccefcc3cab22f7f54616dedb15e4d120ebd54fd7
pmorvalho <pedro.orvalho@tecnico.ulisboa.pt>
2022-02-16 19:20:44 +0000

    updated

--- generator/minimum-sum-coloring/gen_pwcnfs.sh
+++ generator/minimum-sum-coloring/gen_pwcnfs.sh
@@ -10,10 +10,10 @@
 
 n_instances=1000
 per_timeout=150  # we only want at most 15% of timeouts
-# n_min= 25
+# n_min=25
 # n_max=75
 p_min=0.40
-p_max=0.65
+p_max=0.60
 timeouts=0    
 
 #data_dir=$(pwd)
@@ -21,13 +21,15 @@ data_dir="/data/tmp/pwcnfs/minimum-sum-coloring"
 mkdir -p $data_dir $data_dir/outputs $data_dir/graphs $data_dir/pwcnfs-color-based $data_dir/pwcnfs-vertex-based
 
 gen_instances(){
-# $1 is the beginning value for the for-cycle and $2 the limit. 
+# $1 is the beginning value for the for-cycle and $2 the limit.
+# $3 is the current maximum number of nodes and $4 the minimum.
 for((g=$1; g<=$2; g++))
 do  
    echo $g
-   n=$(python -c "import random; print(random.randint("$n_min","$n_max"))")
+   n=$(python -c "import random; print(random.randint($3,$4))")
    p=$(python -c "import random; print(round(random.uniform("$p_min","$p_max"),2))")
    i_name=g$g-n$n-p$(python -c "print(round(100*"$p"))")
+   echo $i_name
    python3 generator.py -n $n -p $p > $data_dir/graphs/$i_name.g
    python3 msc.py -f $data_dir/graphs/$i_name.g > $data_dir/pwcnfs-vertex-based/$i_name.pwcnf
    gzip $data_dir/pwcnfs-vertex-based/$i_name.pwcnf
@@ -59,11 +61,11 @@ done
 }
 
 
-n_min=20
-n_max=30
+n_min=10
+n_max=20
 for((i=1; i<=$n_instances; i=i+50))
 do
-    gen_instances $n_min $n_max &
-    n_min=$((n_min+10))
-    n_max=$((n_max+10))
+    gen_instances $i $((i+50)) $n_min $n_max &
+    n_min=$((n_min+5))
+    n_max=$((n_max+5))
 done
