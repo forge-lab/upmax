a9c9ffeb0c97b69a7a29fa15153994a13007649c
pmorvalho <pedro.orvalho@tecnico.ulisboa.pt>
2022-02-16 20:33:16 +0000

    updated to work with the faster version of vmc.py. Added examples files

--- generator/virtual-machines-consolidation/gen_pwcnfs.sh
+++ generator/virtual-machines-consolidation/gen_pwcnfs.sh
@@ -10,8 +10,8 @@
 
 n_instances=1000
 per_timeout=150  # we only want at most 15% of timeouts
-servers_min=10
-servers_max=40
+# servers_min=10
+# servers_max=40
 vms_min=20
 vms_max=45
 timeouts=0    
@@ -21,29 +21,23 @@ data_dir="/data/tmp/pwcnfs/virtual-machine-consolidation"
 mkdir -p $data_dir $data_dir/outputs $data_dir/instances $data_dir/unweighted/pwcnfs-server-based $data_dir/unweighted/pwcnfs-vm-based $data_dir/weighted/pwcnfs-server-based $data_dir/weighted/pwcnfs-vm-based
 
 gen_instances(){
-# $1 is the beginning value for the for-cycle and $2 the limit. 
+# $1 is the beginning value for the for-cycle and $2 the limit.
+# $3 is the minimum number of servers and $4 the maximum 
 for((g=$1; g<=$2; g++))
 do  
    echo $g
-   s=$(python -c "import random; print(random.randint("$servers_min","$servers_max"))")
+   s=$(python -c "import random; print(random.randint("$3","$4"))")
    u=$(python -c "import random; print(random.randint("$vms_min","$vms_max"))")
    i_name=vmc$g-s$s-u$u
    ./vmc-generator $s $u $g > $data_dir/instances/$i_name.vmc
-   python3 vmc.py -f $data_dir/instances/$i_name.vmc -uw > $data_dir/unweighted/pwcnfs-server-based/$i_name.pwcnf
+   python3 vmc.py -f $data_dir/instances/$i_name.vmc -uw > $data_dir/unweighted/pwcnfs-server-based/$i_name.out
+   clauses=$(tail -n 1 $data_dir/unweighted/pwcnfs-server-based/$i_name.out)
+   vars=$(tail -n 2 $data_dir/unweighted/pwcnfs-server-based/$i_name.out | head -n 1)
+   head -n 1 $data_dir/unweighted/pwcnfs-server-based/$i_name.out | sed -e 's/XX/'$vars' '$clauses'/g' >  $data_dir/unweighted/pwcnfs-server-based/$i_name.pwcnf
+   sed -n 2,$((clauses+1))p $data_dir/unweighted/pwcnfs-server-based/$i_name.out >> $data_dir/unweighted/pwcnfs-server-based/$i_name.pwcnf
+   rm $data_dir/unweighted/pwcnfs-server-based/$i_name.out
    gzip $data_dir/unweighted/pwcnfs-server-based/$i_name.pwcnf
    timeout 600s ../../open-wbo -formula=2 $data_dir/unweighted/pwcnfs-server-based/$i_name.pwcnf.gz > /tmp/$i_name.out
-   # test timrout
-   exit_status=$?
-   if [[ $exit_status -eq 124 ]]; then
-       if [[ $timeout -ge 150 ]]; then # we only want at most 15% of timeouts
-	   rm  $data_dir/instances/$i_name.vmc
-	   rm  $data_dir/unweighted/pwcnfs-server-based/$i_name.pwcnf.gz
-	   i=$((i-1))
-	   continue
-       else
-	   timeout=$((timeout+1))
-       fi
-    fi
    # Test if instance is unsat
    if [[ $(grep "UNSAT" /tmp/$i_name.out) ]]; then
        rm  $data_dir/instances/$i_name.vmc
@@ -61,8 +55,12 @@ do
 done
 }
 
-for((i=1; i<=1000; i=i+100))
+servers_min=1
+servers_max=5
+for((i=1; i<=$n_instances; i=i+50))
 do
-    gen_instances $i $((i+99)) &
+    gen_instances $i $((i+50)) $servers_min $servers_max &
+    servers_min=$((servers_min+2))
+    servers_max=$((servers_max+2))
 done     
 
