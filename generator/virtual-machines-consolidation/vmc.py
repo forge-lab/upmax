1f48c5e684ccf43924c9bd722be34ac1dce19a51
pmorvalho <pedro.orvalho@tecnico.ulisboa.pt>
2022-02-16 20:32:29 +0000

    priting the PWCNF formula line by line with the number of variables and clauses at the end of the file. There is also a flag -cpwnf that prints the complete formula but is quite slower.

--- generator/virtual-machines-consolidation/vmc.py
+++ generator/virtual-machines-consolidation/vmc.py
@@ -3,7 +3,7 @@
 #Usage			: python vmc.py -h
 #Author			: pmorvalho
 #Date			: February 11, 2022
-#Description	        : Encodes the problem of Virtual Machines Consolidation (VMC) into MaxSAT 
+#Description	        : Encodes the problem of Virtual Machines Consolidation (VMC) into MaxSAT\ 
 #Notes			: 
 #Python Version: 3.8.5
 # (C) Copyright 2022 Pedro Orvalho.
@@ -14,11 +14,6 @@ import sys
 from sys import argv
 import argparse
 from pysat.pb import *
-
-# input f
-# g num_nodes num_edges
-# v1 v2
-# ...
 
 parts = 1
 clauses = 0
@@ -74,8 +69,11 @@ def add_lessEqualK_constraint(lits, k, weights=None):
     cnf = PBEnc.leq(lits=lits, top_id=num_vars, weights=weights, bound=k)
     num_vars = cnf.nv
     for cl in cnf.clauses:
-        formula = formula+str(parts)+" "+str(hard)+" "+" ".join([str(c) for c in cl])+" 0\n"
         clauses += 1
+        if not args.complete_pwcnf:
+            print(str(parts)+" "+str(hard)+" "+" ".join([str(c) for c in cl])+" 0")
+        else:
+            formula = formula+str(parts)+" "+str(hard)+" "+" ".join([str(c) for c in cl])+" 0\n"
         
 def encoding():
     global formula, clauses
@@ -101,36 +99,52 @@ def encoding():
     if args.debbug:
         for s in range(1, n_servers+1):
             print("Server {s}: ".format(s=s), servers[s], file=sys.stderr)
-        
+
+    # Header, clauses are going to be defined later for efficiency concerns and variables
+    if not args.complete_pwcnf:
+        print("p pwcnf XX {h} {p}".format(h=hard,p=n_vms+1 if args.pwcnf_vms else n_servers + 1))
+
     # Each VM should assigned to a physical server
     for v in range(1,n_vms+1):
         c = ""
         for s in range(1,n_servers+1):
     	    c = c+" "+str(vms[v][s])
-        formula = formula +str(parts)+" "+str(hard)+c+" 0\n"
         clauses += 1
+        if not args.complete_pwcnf:    
+            print(str(parts)+" "+str(hard)+c+" 0")
+        else:
+            formula = formula +str(parts)+" "+str(hard)+c+" 0\n"
 
     # Each VM should assigned at most one server
     for v in range(1,n_vms+1):
         c = ""
         for s in range(1,n_servers+1):
             for s2 in range(s+1,n_servers+1):
-                formula = formula +str(parts)+" "+str(hard)+" "+ str(-vms[v][s])+" "+ str(-vms[v][s2])+" 0\n"
-                clauses += 1        
+                clauses += 1
+                if not args.complete_pwcnf:    
+                    print(str(parts)+" "+str(hard)+" "+ str(-vms[v][s])+" "+ str(-vms[v][s2])+" 0")
+                else:
+                    formula = formula +str(parts)+" "+str(hard)+" "+ str(-vms[v][s])+" "+ str(-vms[v][s2])+" 0\n"
 
     # If at least one VM is assigned to a server, then that server is active
     for s in range(1,n_servers+1):
         for v in range(1,n_vms+1):
-            formula = formula +str(parts)+" "+str(hard)+" "+ str(-vms[v][s])+" "+ str(servers[s])+" 0\n"
-            clauses += 1        
+            clauses += 1
+            if not args.complete_pwcnf:    
+                print(str(parts)+" "+str(hard)+" "+ str(-vms[v][s])+" "+ str(servers[s])+" 0")
+            else:
+                formula = formula +str(parts)+" "+str(hard)+" "+ str(-vms[v][s])+" "+ str(servers[s])+" 0\n"
 
     # If no VM is assigned to a server, then that server is inactive
     for s in range(1,n_servers+1):
         c = ""
         for v in range(1,n_vms+1):
     	    c = c+" "+str(vms[v][s])
-        formula = formula +str(parts)+" "+str(hard)+c+" "+str(-servers[s])+" 0\n"
-        clauses += 1
+        clauses += 1    
+        if not args.complete_pwcnf:    
+            print(str(parts)+" "+str(hard)+c+" "+str(-servers[s])+" 0")
+        else:
+            formula = formula +str(parts)+" "+str(hard)+c+" "+str(-servers[s])+" 0\n"
             
     # The memory and CPU resources used by the VMs at a given server do not exceed the memory and CPU resources of the server;
     for s in range(1,n_servers+1):
@@ -159,14 +173,22 @@ def encoding():
             if vms_host[v] != s:
                 cost = vms_memory[v] if not args.unweighted else 1  # unweighted version (cost=1)
                 p = v+1 if args.pwcnf_vms else s+1
-                formula = formula + str(p)+" "+str(cost)+" "+ str(-vms[v][s]) + " 0\n"
-                clauses += 1         
+                clauses += 1
+                if not args.complete_pwcnf:
+                    print(str(p)+" "+str(cost)+" "+ str(-vms[v][s]) + " 0")
+                else:
+                    formula = formula + str(p)+" "+str(cost)+" "+ str(-vms[v][s]) + " 0\n"
+
+    if not args.complete_pwcnf:
+        print(num_vars)
+        print(clauses)
                     
                                 
 def parser():
     parser = argparse.ArgumentParser(prog='msc.py', formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-f', '--input_file', nargs='?',help='input file')
     parser.add_argument('-d', '--debbug', action='store_true', default=False, help='Debug info')
+    parser.add_argument('-cpwcnf', '--complete_pwcnf', action='store_true', default=False, help='Writes the complete PWCNF with the value of variables and clauses in the header and not in the end of the file. This approach is quite slower (SLOW).')
     parser.add_argument('-ps', '--pwcnf_servers', action='store_true', default=False, help='Output format: PWCNF (colors) (DEFAULT)')
     parser.add_argument('-pv', '--pwcnf_vms', action='store_true', default=False, help='Output format: PWCNF (nodes)')
     parser.add_argument('-uw', '--unweighted', action='store_true', default=False, help='Unweighted Version of the VMC problem. If this flag is not used then the weighted version is used.')
@@ -177,8 +199,9 @@ def parser():
 def main():
     parse_input(args.input_file)
     encoding()
-    print("p pwcnf {v} {c} {h} {p}".format(v=num_vars,c=clauses,h=hard,p=parts))
-    print(formula, end="")
+    if args.complete_pwcnf:
+        print("p pwcnf {v} {c} {h} {p}".format(v=num_vars,c=clauses,h=hard,p=parts))
+        print(formula, end="")
 
 if __name__ == "__main__":
     args = parser()
