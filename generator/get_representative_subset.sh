2fff9d485a2b05dba1e0f69e007e807c39210425
pmorvalho <pedro.orvalho@tecnico.ulisboa.pt>
2022-11-09 11:43:29 +0000

    representative subset generator updated

--- generator/get_representative_subset.sh
+++ generator/get_representative_subset.sh
@@ -8,16 +8,16 @@
 # (C) Copyright 2022 Pedro Orvalho.
 #==============================================================================
 
-msc_main_dir="/data/tmp/tacas23/minimum-sum-coloring"
-sa_main_dir="/data/tmp/tacas23/seating-assignment"
+# data_dir="/data/tmp/tacas23"
+data_dir=$(pwd)
+msc_main_dir=$data_dir/"evaluation_dataset/minimum-sum-coloring"
+sa_main_dir=$data_dir/"evaluation_dataset/seating-assignment"
 
 # Minimum Sum Coloring (MSC) Problem
-msc_dir="/data/tmp/tacas23/representative_subset/minimum-sum-coloring"
-mkdir -p $msc_dir/"pwcnfs-color-based" $msc_dir/"pwcnfs-vertex-based" $msc_dir/"wcnfs"
+msc_dir=$data_dir/"representative_subset/minimum-sum-coloring"
 
 # Seating Assignment Problem
-sa_dir="/data/tmp/tacas23/representative_subset/seating-assignment"
-mkdir -p $sa_dir"/pwcnfs-tables-based" $sa_dir/"pwcnfs-tags-based" $sa_dir/"wcnfs"
+sa_dir=$data_dir/"representative_subset/seating-assignment"
 
 n_instances=1000
 for((i=1; i<=$n_instances; i=i+50))
@@ -29,13 +29,21 @@ do
     do
 	f=${instances[$j]}
 	# MSC problem
-	cp $msc_main_dir/wcnfs/g$f-* $msc_dir/wcnfs/.
-	cp $msc_main_dir/*color*/g$f-* $msc_dir/*color*/.
-	cp $msc_main_dir/*vertex*/g$f-* $msc_dir/*vertex*/.
+	formulae=("wcnf" "vertex" "color" "vig" "cvig" "res" "random")
+	for((k=0;k<${#formulae[@]};k++));
+	do
+	    formula=${formulae[$k]}
+	    mkdir -p $msc_dir/$formula
+	    cp $msc_main_dir/$formula/g$f-* $msc_dir/$formula/.
+	done
 	
 	# SA problem
-	cp $sa_main_dir/wcnfs/s$f-* $sa_dir/wcnfs/.
-	cp $sa_main_dir/*table*/s$f-* $sa_dir/*table*/.
-	cp $sa_main_dir/*tags*/s$f-* $sa_dir/*tags*/.
+	formulae=("wcnf" "table" "tag" "vig" "cvig" "res" "random")
+	for((l=0;l<${#formulae[@]};l++));
+	do
+	    formula=${formulae[$l]}
+	    mkdir -p $sa_dir/$formula
+	    cp $sa_main_dir/$formula/s$f-* $sa_dir/$formula/.
+	done
     done
 done
