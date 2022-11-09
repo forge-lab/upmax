9fe8e82baa0d7cea0b1d10f903b55c32ef2552c4
pmorvalho <pedro.orvalho@tecnico.ulisboa.pt>
2022-11-09 14:41:33 +0000

    config file

---
+++ config.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#Title			: config.sh
+#Usage			: bash config.sh
+#Author			: pmorvalho
+#Date			: November 09, 2022
+#Description		: Configuration file
+#Notes			: 
+# (C) Copyright 2022 Pedro Orvalho.
+#==============================================================================
+
+# to install PySAT
+pip3 install installation/python-sat-0.1.7.dev19.tar.gz
+
+chmod +x *.sh upPySAT/*.py
+
+unzip solvers/*.zip
